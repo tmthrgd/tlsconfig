3ee80eaa9e753a15592b5da6b7d342fd94373a14
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-03-31 13:29:25 +1030

    Add TLS 1.3 cipher suites for cloudflare/tls-tris

--- ciphers.go
+++ ciphers.go
@@ -23,6 +23,30 @@ var (
 	CipherSuites3DES = cipherSuites(false, true)
 )
 
+const (
+	tls13AES128GCMSHA256        uint16 = 0x1301
+	tls13AES256GCMSHA384        uint16 = 0x1302
+	tls13CHACHA20POLY1305SHA256 uint16 = 0x1303
+)
+
+// TLS13CipherSuites is a preferred list of TLS 1.3 cipher
+// suites  with AES-GCM before ChaCha20-Poly1305. It is
+// intended for use with github.com/cloudflare/tls-tris.
+var TLS13CipherSuites = []uint16{
+	tls13AES128GCMSHA256,
+	tls13AES256GCMSHA384,
+	tls13CHACHA20POLY1305SHA256,
+}
+
+// TLS13CipherSuitesChaCha20 is a preferred list of TLS
+// cipher suites with ChaCha20-Poly1305 before AES-GCM.
+// It is intended for use with github.com/cloudflare/tls-tris.
+var TLS13CipherSuitesChaCha20 = []uint16{
+	tls13CHACHA20POLY1305SHA256,
+	tls13AES128GCMSHA256,
+	tls13AES256GCMSHA384,
+}
+
 func cipherSuites(chaCha20First, threeDES bool) []uint16 {
 	var cipherSuites []uint16
 
@@ -75,7 +99,8 @@ func cipherSuites(chaCha20First, threeDES bool) []uint16 {
 func PrefersChaCha20(chi *tls.ClientHelloInfo) bool {
 	id := chi.CipherSuites[0]
 	return id == tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305 ||
-		id == tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305
+		id == tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305 ||
+		id == tls13CHACHA20POLY1305SHA256
 }
 
 // Should3DES returns true iff 3DES cipher suites
