c2d3bc36e8dc2c3758997a17dce16690c8cfa82b
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-04-12 11:48:27 +0930

    Add Config method to strict

---
+++ strict/config.go
@@ -0,0 +1,50 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+package strict
+
+import (
+	"crypto/tls"
+
+	"github.com/tmthrgd/tlsconfig/internal/tls-tris"
+)
+
+// Config clones config and sets safe, strict defaults.
+func Config(config *tls.Config) *tls.Config {
+	config = config.Clone()
+
+	if config.CipherSuites == nil {
+		config.PreferServerCipherSuites = true
+		config.CipherSuites = CipherSuites
+
+		if !tlstris.SetTLS13CipherSuites(config, TLS13CipherSuites) {
+			// Explicitly disable TLS 1.3 if we were not able
+			// to set the cipher suites.
+			config.MaxVersion = tls.VersionTLS12
+		}
+	}
+
+	// Session Tickets do not refresh the key material
+	// when using TLS 1.2 or earlier. The strict
+	// configuration is intendended for long-lived
+	// connections anyway.
+	config.SessionTicketsDisabled = true
+	config.ClientSessionCache = nil
+
+	if config.MinVersion == 0 {
+		config.MinVersion = tls.VersionTLS12
+	}
+
+	if config.CurvePreferences == nil {
+		config.CurvePreferences = CurvePreferences
+	}
+
+	// Renegotiation is dangerous and the source of bugs
+	// and security issues. It is also useless for the
+	// intended strict use case. Be explicit in disabling it.
+	config.Renegotiation = tls.RenegotiateNever
+
+	return config
+}
