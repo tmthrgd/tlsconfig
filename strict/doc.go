e1cae19190383b727a181d79d7ac004b9c7ba5e6
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-04-12 11:59:28 +0930

    Add package doc to tlsstrict

---
+++ strict/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2017 Tom Thorogood. All rights reserved.
+// Use of this source code is governed by a
+// Modified BSD License license that can be found in
+// the LICENSE file.
+
+// Package tlsstrict provides a sane, common, strict and
+// secure TLS configuration. It is intended for internal
+// communication between private services.
+package tlsstrict
